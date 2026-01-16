4ada543e2a852f50de97bb7b37d010f48bbce993
zKotarou <Lars.Stoermer@hotmail.com>
2026-01-17 00:18:26 +0100

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,7 +1,7 @@
 export const songs = [
   {
-    name: "Vinland Saga - \"MUKANJYO\" by Survive Said The Prophet",
-    videoId: "Z4b70ecDelY",
+    name: "JUJUTSU KAISEN Season 3: The Culling Game Part 1 - \"AIZO\" by King Gnu",
+    videoId: "Xr032EhUDPw",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -10,8 +10,8 @@ export const songs = [
     }
   },
   {
-    name: "Banana Fish - \"found & lost\"   by Survive Said The Prophet",
-    videoId: "VekCU4YJiR8",
+    name: "Frieren: Beyond Journey\u2019s End Season 2 - \"lulu.\" by Mrs.GREEN APPLE",
+    videoId: "C0BG3B7aksU",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -20,8 +20,8 @@ export const songs = [
     }
   },
   {
-    name: "Vinland Saga Season 2 - \"Paradox\"   by Survive Said The Prophet",
-    videoId: "0YT3NTiZKDw",
+    name: "Hell\u2019s Paradise Season 2 - \"Kasukana Hana\" by Tatsuya Kitani feat. BABYMETAL",
+    videoId: "gHIA3Mhc618",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -30,108 +30,8 @@ export const songs = [
     }
   },
   {
-    name: "nan - \"Right and Left\"   by Survive Said The Prophet",
-    videoId: "_B6Pg5Ri7oQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Inside / Your Head\"   by Survive Said The Prophet",
-    videoId: "CtsK3wrqm2A",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Spectrum\"   by Survive Said The Prophet",
-    videoId: "3WJ8CJVVgl8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"S P I N E\"   by Survive Said The Prophet",
-    videoId: "PtiBBkEiHsY",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Conscious\"   by Survive Said The Prophet",
-    videoId: "B1PpXw1VN-k",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Tokyo 24th Ward - \"Papersky\"   by Survive Said The Prophet",
-    videoId: "RolH6t8NOro",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Bridges\"   by Survive Said The Prophet",
-    videoId: "mA3C0VcMdgA",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Mary\"   by Survive Said The Prophet",
-    videoId: "Bt49cLLHVWE",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Useless feat. Ryo Kinoshita\"   by Survive Said The Prophet",
-    videoId: "XVrWwYQNaVA",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"Beauty Queen\"   by Survive Said The Prophet",
-    videoId: "pQoJgfPKCis",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Code Geass Remake Movie - \"NE:ONE\"   by Survive Said The Prophet",
-    videoId: "https://naedist.animemusicquiz.com/lkjzs8.webm",
+    name: "Sentenced to Be a Hero - \"Kill The Noise\" by SPYAIR",
+    videoId: "https://youtu.be/h4I4frynoOo?si=l1WN9ItanNQ_QltN",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -140,8 +40,8 @@ export const songs = [
     }
   },
   {
-    name: "Banana Fish - \"RED\"   by Survive Said The Prophet",
-    videoId: "_rU86Gl7bP8",
+    name: "[Oshi no Ko] 3rd Season - \"B no Revenge\" by B-Komachi",
+    videoId: "5B-ZPcq8KxQ",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -150,18 +50,8 @@ export const songs = [
     }
   },
   {
-    name: "Tokyo 24th Ward - \"Find You\"   by Survive Said The Prophet",
-    videoId: "https://naedist.animemusicquiz.com/bnpod7.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "nan - \"3 A.M.\"   by Survive Said The Prophet",
-    videoId: "jPMOGEe7SQ0",
+    name: "[Oshi no Ko] 3rd Season - \"TEST ME\" by CHANMINA",
+    videoId: "hQ4-H-nNNz4",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -170,8 +60,8 @@ export const songs = [
     }
   },
   {
-    name: "nan - \"If You Really Want To\"   by Survive Said The Prophet",
-    videoId: "JC7K19rWL-k",
+    name: "Fire Force Season 3 Part 2 - \"Ignis\" by Takanori Nishikawa",
+    videoId: "FsQ09FXk_0o",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -180,8 +70,8 @@ export const songs = [
     }
   },
   {
-    name: "nan - \"Mirror\"   by Survive Said The Prophet",
-    videoId: "vAuzMAcuQu0",
+    name: "Fate/strange Fake - \"PROVANT\" by SawanoHiroyuki[nZk]:Jean-Ken Johnny & TAKUMA",
+    videoId: "tcC9g9dlv58",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
@@ -190,8 +80,438 @@ export const songs = [
     }
   },
   {
-    name: "nan - \"Win / Lose\"   by Survive Said The Prophet",
-    videoId: "BUFiuDW0XDI",
+    name: "Chained Soldier Season 2 - \"Hikari yo, Boku ni.\" by Akari Kitou",
+    videoId: "zgLov1TFHL4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "My Hero Academia: Vigilantes Season 2 - \"CATCH!!!\" by Threee",
+    videoId: "bJFqYBcIMcc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Jack-of-All-Trades, Party of None - \"Shirube\" by Towa Tokoyami",
+    videoId: "Jp6G6iiKMBc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "TRIGUN STARGAZE - \"Picaresque Hero\" by ano",
+    videoId: "9PZ3t1RD9Rk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Shibou Yuugi de Meshi wo Kuu. - \"\u00acErsterbend\" by LIN",
+    videoId: "VugBZB4gNGk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "You and I Are Polar Opposites - \"Megane wo Hazushite\" by noa",
+    videoId: "Vr7QV8wDFN0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Tamon's B-Side - \"Sweet Magic\" by F/ACE",
+    videoId: "cugf5FQ4fLE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Tune In to the Midnight Heart - \"Tsuki ni Mukatte Ute\" by Suisei Hoshimachi",
+    videoId: "_sA-cvnwZKQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "There was a Cute Girl in the Hero\u2019s Party, so I Tried Confessing to Her - \"Lavish!!\" by Yoshino",
+    videoId: "vMS0pPsTAkw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Noble Reincarnation: Born Blessed, So I'll Obtain Ultimate Power - \"Break off\" by SUPER\u2605DRAGON",
+    videoId: "iw0oJHZiP30",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Journal with Witch - \"Sonare\" by TOMOO",
+    videoId: "g_nDJVMvSD8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Kunon the Sorcerer Can See - \"Rakenaria no Yume\" by Isekaijoucho",
+    videoId: "e_VgQkPHhDE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Golden Kamuy Final Season - \"Kogane no Kanata\" by Awich \u00d7 ALI",
+    videoId: "YcRYX8hwQ8Y",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "In the Clear Moonlit Dusk - \"Uruwashi\" by UNISON SQUARE GARDEN",
+    videoId: "adm-a1C7TVk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Darwin Incident - \"Make Me Wonder\" by Official HIGE DANdism",
+    videoId: "wTDmqzeugx8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Holy Grail of Eris - \"Happy Ever After feat. YU-KA \" by Reina Washio",
+    videoId: "ozfUpxMXx5k",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "HELL MODE: The Hardcore Gamer Dominates in Another World with Garbage Balancing - \"Haku (\u30cf\u30af)\" by Atarayo",
+    videoId: "XX3rhQgBpiI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Invisible Man and His Soon-to-Be Wife - \"Ding-dong\" by Mao Abe",
+    videoId: "C6sEP_qe3p0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Easygoing Territory Defense by the Optimistic Lord - \"Okirakuzation\" by Rei Nakashima",
+    videoId: "Aooc6j4R8FA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "ROLL OVER AND DIE: I Will Fight for an Ordinary Life with My Love and Cursed Sword! - \"Liberator\" by PassCode",
+    videoId: "CUdMc-TYOIs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Medalist Season 2 - \"Cold Night\" by HANA",
+    videoId: "nHMCtlqzuuc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Wash It All Away - \"Kirei.\" by Yuu.",
+    videoId: "kinVGoEF3O4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Dead Account - \"Dead End\" by Haru Igarashi",
+    videoId: "AidpQPSomnk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "You Can't Be In a Rom-Com with Your Childhood Friends! - \"I Love You \u2661\" by HoneyWorks meets Voice Actors",
+    videoId: "6bCKqFGK5ko",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Isekai Office Worker: The Other World's Books Depend on the Bean Counter - \"Magic\" by kobore",
+    videoId: "d-1B7OkYQJc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Champignon Witch - \"Mahoutsukai no Nikki\" by Rosu",
+    videoId: "FXq7zmbs1ws",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Hana-Kimi - \"Adrena\" by YOASOBI",
+    videoId: "tJI8QWxg0lI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "MF GHOST Season 3 - \"TIMELESS POWER feat. MOTSU\" by Yuu Serizawa",
+    videoId: "Qx-7hPlaCI4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "An Adventurer\u2019s Daily Grind at Age 29 - \"Kimi ga Tomoshite Kureta Hikari wo Ima\" by HoneyWorks feat.Hanon",
+    videoId: "b3-5oQacXKU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Tis Time for \"Torture,\" Princess Season 2 - \"Sunday Morning\" by ILLIT",
+    videoId: "KPSvI5K6O_k",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Villainess Is Adored by the Prince of the Neighbor Kingdom -  \"Ai no Fanfare\" by Ayahi Takagaki feat. Yu Shirota",
+    videoId: "GBs5dPA_iQs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Gentle Noble's Vacation Recommendation - \"Gypso\" by Hikaru Makishima",
+    videoId: "tAJCwoSxojU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Kaya-chan Isn't Scary - \"Maboroshi no Yukue\" by Isekaijoucho",
+    videoId: "qAlPo5EHilg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Reincarnated as a Dragon Hatchling - \"Gliding Claw\" by Sizuk",
+    videoId: "tBqY4LcRsrE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Scum of the Brave - \"GUN POWDER\" by TOOBOE",
+    videoId: "0uwmTsnMSV0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Case Book of Arne - \"Q.E.D.\" by Sou",
+    videoId: "CebPc_NsjiI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "DARK MOON: THE BLOOD ALTAR - \"One In A Billion\" by ENHYPEN",
+    videoId: "aiAUHgVtGH0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Misanthrope Teaches a Class for Demi-Humans - \"Ningen\" by Masayoshi Oishi",
+    videoId: "RLVTVMsb6Ic",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Yoroi-Shinden Samurai Troopers - \"YOAKE\" by blank paper",
+    videoId: "kHUPdA8H6qk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Hell Teacher: Jigoku Sensei Nube (2025) Part 2 - \"ERASE\" by THE ORAL CIGARETTES",
+    videoId: "hYx3JeSqDYg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Cardfight!! Vanguard: Divinez Genma Seisen-hen - \"Genkai\" by V.W.P",
+    videoId: "zCbiWxWN1dE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Anyway, I'm Falling in Love with You. Season 2 - \"L to R (L\u3068R)\" by ME:I",
+    videoId: "0Lxu3LIY-m4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Daily Life of a Part-time Torturer - \"GO GO PARADISE!!\" by GRANRODEO",
+    videoId: "KOhrAtk6BLw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "SI-VIS: The Sound of Heroes - \"Supernova\" by Si-Vis",
+    videoId: "g4mxnycOiMs",
     videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
