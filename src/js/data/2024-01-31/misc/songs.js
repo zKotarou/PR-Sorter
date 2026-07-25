d1ceebcacd72ffe9c6c35500cc776ec14699d974
zKotarou <Lars.Stoermer@hotmail.com>
2026-07-25 21:55:24 +0200

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,27 +1,7 @@
 export const songs = [
   {
-    name: "One Piece - \"We Are!\" by Hiroshi Kitadani",
-    videoId: "https://naedist.animemusicquiz.com/c7pqug.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
     name: "One Piece - \"Believe\" by Folder5",
     videoId: "https://naedist.animemusicquiz.com/im1a6p.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"Hikari e\" by The Babystars",
-    videoId: "https://naedist.animemusicquiz.com/tlds4l.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -40,16 +20,6 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Kokoro no Chizu\" by BOYSTYLE",
-    videoId: "https://naedist.animemusicquiz.com/a0g48o.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
     name: "One Piece - \"BRAND NEW WORLD\" by D-51",
     videoId: "https://naedist.animemusicquiz.com/aemf9t.webm",
     videoSource: VIDEOSOURCE.CATBOX,
@@ -60,8 +30,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"We Are! \" by the 7-nin no Mugiwara Kaizokudan",
-    videoId: "https://naedist.animemusicquiz.com/zjypxh.webm",
+    name: "One Piece - \"Carmine\" by ELLEGARDEN",
+    videoId: "https://naedist.animemusicquiz.com/cpk6za7n0ir1nx3y.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -80,48 +50,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Jungle P\" by 5050",
-    videoId: "https://naedist.animemusicquiz.com/dh2ojv.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"We Are!~Animation One Piece 10 Shuunen Ver.~\" by TVXQ",
-    videoId: "https://naedist.animemusicquiz.com/bwx8pb.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"Share The World\" by TVXQ",
-    videoId: "https://naedist.animemusicquiz.com/zdxhyr.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"Kaze wo Sagashite\" by Mari Yaguchi to Straw Hat",
-    videoId: "https://naedist.animemusicquiz.com/vw23ce.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"One day\" by The ROOTLESS ",
-    videoId: "https://naedist.animemusicquiz.com/xadig3.webm",
+    name: "One Piece - \"DREAMIN' ON\" by Da-iCE",
+    videoId: "https://naedist.animemusicquiz.com/hnfwlu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -140,28 +70,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"We Go! \" by Hiroshi Kitadani ",
-    videoId: "https://naedist.animemusicquiz.com/io0s7q.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
     name: "One Piece - \"HANDS UP!\" by Kouta Shinzato",
     videoId: "https://naedist.animemusicquiz.com/bjm0na.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"Wake up!\" by AAA",
-    videoId: "https://naedist.animemusicquiz.com/7ghiql.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -180,8 +90,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"We Can!\" by Kishidan to Hiroshi Kitadani",
-    videoId: "https://naedist.animemusicquiz.com/acof2e.webm",
+    name: "One Piece - \"Hikari e\" by The Babystars",
+    videoId: "https://naedist.animemusicquiz.com/tlds4l.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -200,8 +110,48 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Super Powers\" by V6",
-    videoId: "https://naedist.animemusicquiz.com/7f33tu.webm",
+    name: "One Piece - \"Jungle P\" by 5050",
+    videoId: "https://naedist.animemusicquiz.com/dh2ojv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"Kaze wo Sagashite\" by Mari Yaguchi to Straw Hat",
+    videoId: "https://naedist.animemusicquiz.com/vw23ce.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"Kokoro no Chizu\" by BOYSTYLE",
+    videoId: "https://naedist.animemusicquiz.com/a0g48o.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"Luminous\" by AiNA THE END",
+    videoId: "https://naedist.animemusicquiz.com/evd8bv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"One day\" by The ROOTLESS ",
+    videoId: "https://naedist.animemusicquiz.com/xadig3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -212,16 +162,6 @@ export const songs = [
   {
     name: "One Piece - \"OVER THE TOP\" by Hiroshi Kitadani",
     videoId: "https://naedist.animemusicquiz.com/apxeiw.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "One Piece - \"DREAMIN' ON\" by Da-iCE",
-    videoId: "https://naedist.animemusicquiz.com/hnfwlu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -250,8 +190,28 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Uuus!\" by Hiroshi Kitadani",
-    videoId: "https://naedist.animemusicquiz.com/fsfc2w.webm",
+    name: "One Piece - \"Share The World\" by TVXQ",
+    videoId: "https://naedist.animemusicquiz.com/zdxhyr.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece Film: Red - \"Shinjidai\" by Ado",
+    videoId: "https://naedist.animemusicquiz.com/uwbwsc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"Super Powers\" by V6",
+    videoId: "https://naedist.animemusicquiz.com/7f33tu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -270,8 +230,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Carmine\" by ELLEGARDEN",
-    videoId: "https://naedist.animemusicquiz.com/cpk6za7n0ir1nx3y.webm",
+    name: "One Piece - \"Uuus!\" by Hiroshi Kitadani",
+    videoId: "https://naedist.animemusicquiz.com/fsfc2w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -280,8 +240,8 @@ export const songs = [
     }
   },
   {
-    name: "One Piece - \"Luminous\" by AiNA THE END",
-    videoId: "https://naedist.animemusicquiz.com/evd8bv.webm",
+    name: "One Piece - \"Wake up!\" by AAA",
+    videoId: "https://naedist.animemusicquiz.com/425x16.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -290,8 +250,48 @@ export const songs = [
     }
   },
   {
-    name: "One Piece Film: Red - \"Shinjidai\" by Ado",
-    videoId: "https://naedist.animemusicquiz.com/uwbwsc.webm",
+    name: "One Piece - \"We Are! \" by the 7-nin no Mugiwara Kaizokudan",
+    videoId: "https://naedist.animemusicquiz.com/zjypxh.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"We Are!\" by Hiroshi Kitadani",
+    videoId: "https://naedist.animemusicquiz.com/c7pqug.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"We Are!~Animation One Piece 10 Shuunen Ver.~\" by TVXQ",
+    videoId: "https://naedist.animemusicquiz.com/bwx8pb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"We Can!\" by Kishidan to Hiroshi Kitadani",
+    videoId: "https://naedist.animemusicquiz.com/acof2e.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "One Piece - \"We Go! \" by Hiroshi Kitadani ",
+    videoId: "https://naedist.animemusicquiz.com/io0s7q.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
